10a8dded83fadb7103e0eceb0b5c4b186d832463
Santosh <santykr7@gmail.com>
2023-12-12 02:43:25 +0530

    reusable Component and useref

--- megaBlog/src/App.jsx
+++ megaBlog/src/App.jsx
@@ -1,15 +1,38 @@
+import { useEffect, useState } from "react"
+import { useDispatch } from "react-redux"
 import "./App.css"
+import authService from "./appwrite/auth"
+import { login, logout } from "./store/authSlice"
+import { Footer, Header } from "./Component"
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL)
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
 
-  return (
-    <>
-      <div>
-        <h1>Mega Block</h1>
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }))
+        } else {
+          dispatch(logout())
+        }
+      })
+      .finally(() => setLoading(false))
+  }, [])
+
+  // condition loading on screen
+
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-500'>
+      <div className='w-full block'>
+        <Header />
+        <main>TODO{/* outlet handle */}</main>
+        <Footer />
       </div>
-    </>
-  )
+    </div>
+  ) : null
 }
 
 export default App
---
+++ megaBlog/src/store/authSlice.js
@@ -0,0 +1,23 @@
+import { createSlice } from "@reduxjs/toolkit"
+
+const initialState = {
+  status: false,
+  UserData: null,
+}
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    login: (state, action) => {
+      ;(state.status = true), (state.UserData = action.payload)
+    },
+    logout: (state) => {
+      ;(state.status = false), (state.UserData = null)
+    },
+  },
+})
+
+export const { login, logout } = authSlice.actions
+
+export default authSlice.reducer
